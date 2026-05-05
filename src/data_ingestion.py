af8127cfb18a58886ea910602e048c114c10ba4b
AshleshaNemade <ashnemade16@gmail.com>
2026-05-05 22:26:39 +0530

    add preprocessing, models, evaluation, and training code

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -4,5 +4,5 @@ def load_data(path):
     return pd.read_csv(path)
 
 if __name__ == "__main__":
-    df = load_data("data/raw/WA_Fn-UseC_-Telco-Customer-Churn.csv")
+    df = load_data("data/raw/TelcoCustomer_Churn.csv")
     print(df.head())
